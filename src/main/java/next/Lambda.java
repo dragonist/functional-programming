b1838dd4ad80a176313e8b84487e45e2bcaab588
dragonist <erin314@naver.com>
2015-09-03 16:27:01 +0900

    #finish

--- src/main/java/next/Lambda.java
+++ src/main/java/next/Lambda.java
@@ -1,6 +1,7 @@
 package next;
 
 import java.util.List;
+import java.util.function.Predicate;
 
 public class Lambda {
 	public static void printAllOld(List<Integer> numbers) {
@@ -18,39 +19,37 @@ public class Lambda {
 	}
 	
 	public static void runThread() {
-		new Thread(new Runnable() {
-		    @Override
-		    public void run() {
-		        System.out.println("Hello from thread");
-		    }
-		}).start();
+		new Thread(() ->System.out.println("Hello from thread")).start();
+//		new Thread(new Runnable() {
+//		    @Override
+//		    public void run() {
+//		        System.out.println("Hello from thread");
+//		    }
+//		}).start();
 	}
 
-	public static int sumAll(List<Integer> numbers) {
-	    int total = 0;
+	
+	public static int sum(List<Integer> numbers, Predicate<Integer> p){
+		int total = 0;
+	    
 	    for (int number : numbers) {
+	    	if(p.test(number)){
 	        total += number;
+	    	}
 	    }
 	    return total;
 	}
 	
+	public static int sumAll(List<Integer> numbers) {
+		return sum(numbers,(number)-> true);
+	}
+	//여기서 (number)->true는 인터페이스의 구현하지 않은 부분이다. 다른 메서드에 인터페이스의 메서드를 구현해서 전달한다.
+	
 	public static int sumAllEven(List<Integer> numbers) {
-	    int total = 0;
-	    for (int number : numbers) {
-	        if (number % 2 == 0) {
-	            total += number;
-	        }
-	    }
-	    return total;
+		return sum(numbers,(number)->(number % 2 == 0));
 	}
 
 	public static int sumAllOverThree(List<Integer> numbers) {
-	    int total = 0;
-	    for (int number : numbers) {
-	    	if (number > 3) {
-	    		total += number;
-	    	}
-	    }
-	    return total;
+		return sum(numbers,(number)->(number > 3));
 	}
 }
--- src/main/java/next/StreamStudy.java
+++ src/main/java/next/StreamStudy.java
@@ -8,25 +8,31 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import ch.qos.logback.core.net.SyslogOutputStream;
+
 public class StreamStudy {
 
 	public static long countWords() throws IOException {
-		String contents = new String(Files.readAllBytes(Paths
-				.get("src\\main\\resources\\war-and-peace.txt")), StandardCharsets.UTF_8);
+		String contents = new String(Files.readAllBytes(Paths.get("src/main/resources/war-and-peace.txt")),
+				StandardCharsets.UTF_8);
 		List<String> words = Arrays.asList(contents.split("[\\P{L}]+"));
 
 		long count = 0;
 		for (String w : words) {
-		  if (w.length() > 12) count++;  
+			if (w.length() > 12)
+				count++;
 		}
 		return count;
 	}
-	
+
 	public static void printLongestWordTop100() throws IOException {
-		String contents = new String(Files.readAllBytes(Paths
-				.get("src\\main\\resources\\war-and-peace.txt")), StandardCharsets.UTF_8);
+		String contents = new String(Files.readAllBytes(Paths.get("src/main/resources/war-and-peace.txt")),
+				StandardCharsets.UTF_8);
 		List<String> words = Arrays.asList(contents.split("[\\P{L}]+"));
-		
+		words = words.stream().filter(x -> x.length() > 12).map(x -> x.toLowerCase()).distinct().sorted((x,y)-> y.length() - x.length()).limit(100)
+				.collect(Collectors.toList());
+
+		words.forEach(System.out::println);
 		// TODO 이 부분에 구현한다.
 	}
 
@@ -39,6 +45,6 @@ public class StreamStudy {
 	}
 
 	public static long sumOverThreeAndDouble(List<Integer> numbers) {
-		return 0;
+		return numbers.stream().filter(x -> (x > 3)).map(x -> 2 * x).reduce(0, (x, y) -> x + y);
 	}
 }
